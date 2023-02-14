6f365b607aa7080e07857632d6fb20fac838ec07
Mateo Rodriguez <luismateor73@gmail.com>
2023-02-13 21:26:14 -0500

    Confirmar Usuario - Comprobar JWT

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -54,4 +54,23 @@ const autenticar = async (req, res)=>{
     }
 }
 
-export { registrar, autenticar }
+const confirmar = async (req, res)=>{
+    const { token } = req.params
+    const usuarioConfirmar = await Usuario.findOne({ token })
+    
+    if(!usuarioConfirmar){
+        const error = new Error('Token no válido')
+        return res.status(403).json({ msg: error.message })
+    }
+
+    try {
+        usuarioConfirmar.confirmado = true
+        usuarioConfirmar.token = ''
+        await usuarioConfirmar.save()
+        res.json({ msg: 'Usuario Confirmado Correctamente'})
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export { registrar, autenticar, confirmar }
