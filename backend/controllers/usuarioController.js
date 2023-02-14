7d03707024dbadfa44776f9cd1091e9d75988cee
Mateo Rodriguez <luismateor73@gmail.com>
2023-02-13 21:28:12 -0500

    Pedir recuperación de Password

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -73,4 +73,25 @@ const confirmar = async (req, res)=>{
     }
 }
 
-export { registrar, autenticar, confirmar }
+const olvidePassword = async (req, res)=>{
+
+const { email } = req.body;
+
+    //Comprobar si el usuario existe
+    const usuario = await Usuario.findOne({ email });
+    if(!usuario){
+        const error = new Error('El usuario no exsite');
+        return res.status(404).json({ msg:  error.message })
+    }
+
+    try {
+        usuario.token = generarId()
+        await usuario.save();
+
+        res.json({ msg: 'Hemos enviado un email con las instrucciones' })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export { registrar, autenticar, confirmar, olvidePassword }
