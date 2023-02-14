ad116a37485ad3e4800c9794e99a163eda0f3921
Mateo Rodriguez <luismateor73@gmail.com>
2023-02-14 03:25:52 -0500

    Modelado de las Tareas con mongoose

---
+++ backend/models/Tarea.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const tareasSchema = mongoose.Schema({
+    nombre:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    descripcion:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    estado:{
+        type: Boolean,
+        default: false
+    },
+    fechaEntrega:{
+        type: Date,
+        default: Date.now(),
+        required: true
+    },
+    prioridad:{
+        type: String,
+        required: true,
+        enum: ['Baja', 'Media', 'Alta']
+    },
+    proyecto:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Proyecto"
+    }
+},
+{
+    timestamps: true
+})
+
+const Tarea = mongoose.model("Tarea", tareasSchema)
+export default Tarea;
