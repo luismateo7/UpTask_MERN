9bd1a900af0ae9c7d15427c404a7572a9dc96700
Mateo Rodriguez <luismateor73@gmail.com>
2023-02-13 23:34:15 -0500

    Routes para CRUD Proyectos

---
+++ backend/routes/proyectoRoutes.js
@@ -0,0 +1,32 @@
+import express from 'express';
+const router = express.Router();
+
+import{
+    obtenerProyectos,
+    obtenerProyecto,
+    nuevoProyecto,
+    editarProyecto,
+    eliminarProyecto,
+    agregarColaborador,
+    eliminarColaborador,
+    obtenerTareas
+
+} from "../controllers/proyectoController.js"
+import checkAuth from "../middleware/checkAuth.js"
+
+router
+    .route('/')
+    .get(checkAuth, obtenerProyectos)
+    .post(checkAuth, nuevoProyecto)
+
+router
+    .route('/:id')
+    .get(checkAuth, obtenerProyecto)
+    .put(checkAuth, editarProyecto)
+    .delete(checkAuth, eliminarProyecto)
+
+router.get('/tareas/:id', checkAuth, obtenerTareas)
+router.post('/agregar-colaborador/:id', checkAuth, agregarColaborador)
+router.post('/eliminar-colaborador/:id', checkAuth, eliminarColaborador)
+
+export default router;
