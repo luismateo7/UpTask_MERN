c43d8c1a723289fb7f8de56186382120d9e3f733
Mateo Rodriguez <luismateor73@gmail.com>
2023-02-13 23:33:25 -0500

    Generar un Nuevo Proyecto

---
+++ backend/controllers/proyectoController.js
@@ -0,0 +1,53 @@
+import Proyecto from "../models/Proyecto.js";
+
+const obtenerProyectos = async (req, res) => {
+    
+}
+
+const obtenerProyecto = async (req, res) => {
+
+}
+
+const nuevoProyecto = async (req, res) => {
+    const proyecto = new Proyecto(req.body); //Crea un nuevo proyecto
+    proyecto.creador = req.usuario._id; //Se le asigna su creado
+
+    try {
+        const proyectoAlmacenado = await proyecto.save(); //Se almacena
+        res.json(proyectoAlmacenado);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const editarProyecto = async (req, res) => {
+
+}
+
+const eliminarProyecto = async (req, res) => {
+
+}
+
+const agregarColaborador = async (req, res) => {
+
+}
+
+const eliminarColaborador = async (req, res) => {
+
+}
+
+const obtenerTareas = async (req, res) => {
+
+}
+
+
+export{
+    obtenerProyectos,
+    obtenerProyecto,
+    nuevoProyecto,
+    editarProyecto,
+    eliminarProyecto,
+    agregarColaborador,
+    eliminarColaborador,
+    obtenerTareas
+}
