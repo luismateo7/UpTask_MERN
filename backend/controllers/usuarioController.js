a597ac8963a7638239e5190143e5a337d96765db
Mateo Rodriguez <luismateor73@gmail.com>
2023-02-13 21:30:54 -0500

    Crear nuevo password

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -107,4 +107,29 @@ const comprobarToken = async (req, res)=>{
     }
 }
 
-export { registrar, autenticar, confirmar, olvidePassword, comprobarToken }
+const nuevoPassword = async (req, res)=>{
+    const { token } = req.params;
+    const { password } = req.body;
+
+    const usuario = await Usuario.findOne( { token });
+    
+    if(usuario){ 
+        usuario.password = password;
+        usuario.token = '';
+
+        try {
+            await usuario.save()
+            res.json({ msg: 'Password modificado correctamente'})
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    else{ 
+        const error = new Error('Token no válido');
+        return res.status(404).json({ msg:  error.message });
+    }
+
+}
+
+export { registrar, autenticar, confirmar, olvidePassword, comprobarToken, nuevoPassword }
