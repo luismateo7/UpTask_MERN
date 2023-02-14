d4e065e62a4d212e3516e01ccd7d3421db3498e6
Mateo Rodriguez <luismateor73@gmail.com>
2023-02-14 04:10:25 -0500

    Obtener una tarea y validaciones

--- backend/controllers/tareaController.js
+++ backend/controllers/tareaController.js
@@ -13,7 +13,7 @@ const agregarTarea = async (req, res) => {
 
     if(existeProyecto.creador.toString() !== req.usuario._id.toString()){
         const error = new Error("No tiene los permisos para añadir las tareas")
-        return res.status(401).json({ msg: error.message})
+        return res.status(403).json({ msg: error.message})
     }
 
     try {
@@ -25,7 +25,21 @@ const agregarTarea = async (req, res) => {
 }
 
 const obtenerTarea = async (req, res) => {
+    const { id } = req.params;
 
+    const tarea = await Tarea.findById(id).populate("proyecto");
+    
+    if(!tarea){
+        const error = new Error("La tarea no existe")
+        return res.status(404).json({ msg: error.message})
+    }
+
+    if(tarea.proyecto.creador.toString() !== req.usuario._id.toString()){
+        const error = new Error("No tiene los permisos para acceder a esta tarea")
+        return res.status(403).json({ msg: error.message})
+    }
+
+    res.json(tarea)
 }
 
 const actualizarTarea = async (req, res) => {
