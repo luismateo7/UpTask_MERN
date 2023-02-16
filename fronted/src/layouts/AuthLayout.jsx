5d20caafdb663794cdbcf678b48f00820c76be76
Mateo Rodriguez <luismateor73@gmail.com>
2023-02-15 23:19:10 -0500

    Layout de rutas públicas

---
+++ fronted/src/layouts/AuthLayout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom"
+
+export default function AuthLayout() {
+  return (
+    <>
+      <main className="container mx-auto mt-5 md:mt-8 p-5 md:flex md:justify-center">
+        <div className="md:w-2/3 lg:w-2/5">
+          <Outlet />
+        </div>
+      </main>  
+    </>
+  )
+}
