cb764aa544752390b2cfc6ddb9fc16693497887c
Mateo Rodriguez <luismateor73@gmail.com>
2023-02-15 23:46:41 -0500

    State local a los campos del Form y validacion

--- fronted/src/pages/Registrar.jsx
+++ fronted/src/pages/Registrar.jsx
@@ -1,13 +1,62 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
 
+import Alerta from "../components/Alerta";
+
 export default function Registrar() {
+
+  const [ nombre, setNombre ] = useState('');
+  const [ email, setEmail ] = useState('');
+  const [ password, setPassword ] = useState('');
+  const [ repetirPassword, setRepetirPassword ] = useState('');
+  
+  const [ alerta, setAlerta ] = useState({})
+
+  const handleSubmit = e =>{
+    e.preventDefault();
+
+    setAlerta({})
+
+    if([ nombre, email, password, repetirPassword ].includes('')){
+      setAlerta({
+        msg: 'Todos los campos son obligatorios',
+        error: true
+      })
+      return
+    }
+
+    if(password !== repetirPassword){
+      setAlerta({
+        msg: 'Los password no son iguales',
+        error: true
+      })
+      return
+    }
+
+    if(password.length < 6){
+      setAlerta({
+        msg: 'El password es muy corto, agrega mínimo 6 caracteres',
+        error: true
+      })
+      return
+    }
+
+  }
+  
+  const { msg } = alerta
+
   return (
     <>
       <h1 className="text-sky-600 font-black text-6xl capitalize">Crea tu cuenta y administra tus
         <span className="text-slate-700"> proyectos</span>
       </h1>
 
-      <form className="my-10 bg-white shadow rounded-lg py-5 px-10">
+      { msg && <Alerta alerta={alerta}/>}
+
+      <form 
+        className="my-10 bg-white shadow rounded-lg py-5 px-10"
+        onSubmit={handleSubmit}
+      >
         <div className="my-5">
           <label
             htmlFor="nombre"
@@ -19,6 +68,8 @@ export default function Registrar() {
             placeholder="Tu nombre"
             className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
             id="nombre"
+            value={nombre}
+            onChange={e => setNombre(e.target.value)}
           />
         </div>
 
@@ -33,6 +84,8 @@ export default function Registrar() {
             placeholder="Email de Registro"
             className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
             id="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
           />
         </div>
 
@@ -47,6 +100,8 @@ export default function Registrar() {
             placeholder="Password de Registro"
             className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
             id="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
           />
         </div>
 
@@ -57,10 +112,12 @@ export default function Registrar() {
           >Repetir password</label>
           
           <input
-            type="password2" 
+            type="password" 
             placeholder="Repetir tu password"
             className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
             id="password2"
+            value={repetirPassword}
+            onChange={e => setRepetirPassword(e.target.value)}
           />
         </div>
 
