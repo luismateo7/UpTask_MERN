7bac287e498fcd517b3f31fb91ddb00605819979
Mateo Rodriguez <luismateor73@gmail.com>
2023-02-16 01:25:51 -0500

    Instalación Y Configuración de CORS

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import conectarDB from "./config/db.js";
 import dotenv from "dotenv";
+import cors from "cors";
+
 import usuarioRoutes from "./routes/usuarioRoutes.js"
 import proyectoRoutes from "./routes/proyectoRoutes.js"
 import tareaRoutes from "./routes/tareaRoutes.js"
@@ -10,6 +12,22 @@ app.use(express.json())
 
 dotenv.config()
 conectarDB()
+
+//Configurar CORS
+const whiteList = [process.env.FRONTED_URL]; //Fronted permitidos
+
+const corsOption = {
+    origin: function(origin, callback){ //El origin detecta que fronted esta realizando la peticion
+        if(whiteList.includes(origin)){ //Si este origin que hace la peticion esta en whiteList entonces puede hacer la consulta a la API
+            callback(null, true); //Damos acceso
+        } else {
+            //No esat permitido
+            callback(new Error("Error de Cors"))
+        }
+    }
+}
+
+app.use(cors(corsOption))
 
 //Routing
 app.use('/api/usuarios', usuarioRoutes)
