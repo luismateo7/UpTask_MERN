d0e3cba4cc17569602ef4bf148998eaf39b0a8ad
Mateo Rodriguez <luismateor73@gmail.com>
2023-02-18 02:39:18 -0500

    Obtener los proyectos el usuario autenticado

--- fronted/src/context/ProyectosProvider.jsx
+++ fronted/src/context/ProyectosProvider.jsx
@@ -1,26 +1,46 @@
 import { useState, useEffect, createContext } from "react";
+import { redirect, useNavigate } from "react-router-dom";
 import axios from "axios"
 
 const ProyectosContext = createContext();
 
 const ProyectosProvider = ({children}) =>{
 
+    const navigate = useNavigate();
     const [ proyectos, setProyectos ] = useState([]);
+
+    const token = localStorage.getItem('token');
+    
+    const config = {
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        }
+    }
+
+    useEffect(()=>{
+        const obtenerProyectos = async ()=>{
+            try {
+                if(!token) return
+                const { data } = await axios(`${import.meta.env.VITE_BACKEND_URL}/api/proyectos`, config);
+                setProyectos(data);
+                
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        obtenerProyectos()
+    }, [])
 
     const submitProyecto = async proyecto =>{
         try {
-            console.log(proyecto);
-            const token = localStorage.getItem('token');
             if(!token) return
-            
-            const config = {
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${token}`
-                }
-            }
-
             const { data } = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/proyectos`, proyecto, config);
+            setProyectos([...proyectos, data])
+
+            setTimeout(()=>{
+                navigate('/proyectos')
+            }, 1500)
 
         } catch (error) {
             console.log(error);
