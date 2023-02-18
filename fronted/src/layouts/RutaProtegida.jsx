0d5c3c561b759eb435d645df1838d80ad463abd4
Mateo Rodriguez <luismateor73@gmail.com>
2023-02-18 01:41:08 -0500

    Mostrar si el user esta autenticado sino al login

--- fronted/src/layouts/RutaProtegida.jsx
+++ fronted/src/layouts/RutaProtegida.jsx
@@ -1,13 +1,33 @@
 import { Outlet, Navigate } from "react-router-dom"
 import useAuth from "../hooks/useAuth"
 
+import Header from "../components/Header"
+import Sidebar from "../components/Sidebar"
+
 export default function RutaProtegida() {
 
-  const { auth } = useAuth();
+  const { auth, cargando } = useAuth();
+
+  if(cargando) return 'Cargando...'
+
   return (
     <>
-      {auth._id ? '' : <Navigate to='/'/> }
-      <Outlet />
+      {auth._id ? (
+        <div className="bg-gray-100">
+
+          <Header />
+
+          <div className="md:flex md:min-h-screen">
+            <Sidebar />
+
+            <main className="flex-1 p-10">
+              <Outlet />
+            </main>
+
+          </div>
+
+        </div>
+      ) : <Navigate to='/'/> }
     </>
   )
 }
