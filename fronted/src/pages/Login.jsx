940c5108ad8fb126735bd32db4bf603f8073dea5
Mateo Rodriguez <luismateor73@gmail.com>
2023-02-17 20:11:08 -0500

    Autenticar el Login y validaciones

--- fronted/src/pages/Login.jsx
+++ fronted/src/pages/Login.jsx
@@ -1,13 +1,58 @@
-import { Link } from "react-router-dom"
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import Alerta from "../components/Alerta"
+import axios from "axios"
 
 export default function Login() {
+
+  const [ email, setEmail ] = useState('');
+  const [ password, setPassword ] = useState('');
+  const [ alerta, setAlerta ] = useState({});
+
+  const handleSubmit = async e =>{
+    e.preventDefault();
+
+    if([ email, password ].includes('')){
+      setAlerta({
+        msg: 'Todos los campos son obligatorios',
+        error: true
+      })
+      return
+    }
+
+    try {
+      //Accedo directamente al req.body gracias al destructuring a data
+      const { data } = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/usuarios/login`, { email, password }); 
+
+      localStorage.setItem('token', data.token);
+
+      setAlerta({
+        msg: data.msg,
+        error: false
+      })
+
+    } catch (error) {
+      setAlerta({
+        msg: error.response.data.msg,
+        error: true
+      })
+    }
+  }
+
+  const { msg } = alerta;
+
   return (
     <>
       <h1 className="text-sky-600 font-black text-6xl capitalize">Inicia sesión y administra tus
         <span className="text-slate-700"> proyectos</span>
       </h1>
 
-      <form className="my-10 bg-white shadow rounded-lg py-5 px-10">
+      { msg && <Alerta alerta={alerta}/>}
+
+      <form 
+        className="my-10 bg-white shadow rounded-lg py-5 px-10"
+        onSubmit={handleSubmit}
+      >
         <div className="my-5">
           <label
             htmlFor="email"
@@ -19,6 +64,8 @@ export default function Login() {
             placeholder="Email de Registro"
             className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
             id="email"
+            value={email}
+            onChange={ e => setEmail(e.target.value)}
           />
         </div>
 
@@ -33,6 +80,8 @@ export default function Login() {
             placeholder="Password de Registro"
             className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
             id="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
           />
         </div>
 
