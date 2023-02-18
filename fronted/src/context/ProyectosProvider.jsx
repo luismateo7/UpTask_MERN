6a6890c1dd748a9a46b083dcccef9149ddc67d8e
Mateo Rodriguez <luismateor73@gmail.com>
2023-02-18 01:40:30 -0500

    Crear el proyecto en API

--- fronted/src/context/ProyectosProvider.jsx
+++ fronted/src/context/ProyectosProvider.jsx
@@ -1,15 +1,37 @@
 import { useState, useEffect, createContext } from "react";
+import axios from "axios"
 
 const ProyectosContext = createContext();
 
 const ProyectosProvider = ({children}) =>{
 
-    const [ proyectos, setProyectos ] = useState('Hola');
+    const [ proyectos, setProyectos ] = useState([]);
+
+    const submitProyecto = async proyecto =>{
+        try {
+            console.log(proyecto);
+            const token = localStorage.getItem('token');
+            if(!token) return
+            
+            const config = {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`
+                }
+            }
+
+            const { data } = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/proyectos`, proyecto, config);
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
     return(
         <ProyectosContext.Provider
             value={{
-                proyectos
+                proyectos,
+                submitProyecto
             }}
         >
             {children}
