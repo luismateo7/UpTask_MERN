5ba30725823eba94774836818f6774924accf6af
Mateo Rodriguez <luismateor73@gmail.com>
2023-02-17 20:58:30 -0500

    Comprobar si el user esta auth cuando carga la web

--- fronted/src/context/AuthProvider.jsx
+++ fronted/src/context/AuthProvider.jsx
@@ -1,12 +1,42 @@
 import { useState, useEffect, createContext } from "react";
+import axios from "axios";
 
 const AuthContext = createContext();
 
 const AuthProvider = ({children})=>{
+
+    const [ auth, setAuth ] = useState({});
+
+    useEffect(()=>{
+        const autenticarUsuario = async ()=>{
+            const token = localStorage.getItem('token');
+
+            if(!token) return;
+
+            const config = {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`
+                }
+            }
+
+            try {
+                const { data } = await axios(`${import.meta.env.VITE_BACKEND_URL}/api/usuarios/perfil`, config)
+
+                setAuth(data);
+            } catch (error) {
+                
+            }
+        }
+        
+        autenticarUsuario()
+    }, [])
+
     return(
         <AuthContext.Provider
             value={{
-
+                auth,
+                setAuth
             }}
         >
             {children}
