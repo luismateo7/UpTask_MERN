3a4ba554d9c9f440b6349ba2536a4c363bef58d4
Mateo Rodriguez <luismateor73@gmail.com>
2023-02-17 20:10:29 -0500

    Cmabiar el password en la API y validaciones

--- fronted/src/pages/NuevoPassword.jsx
+++ fronted/src/pages/NuevoPassword.jsx
@@ -5,6 +5,10 @@ import Alerta from "../components/Alerta"
 
 export default function NuevoPassword() {
 
+  const [ password, setPassword ] = useState('');
+  const [ repetirPassword, setRepetirPassword ] = useState('');
+  const [ passwordModificado, setPasswordModificado ] = useState(false);
+
   const [ alerta, setAlerta ] = useState({});
   const [ tokenValido, setTokenValido ] = useState(false);
 
@@ -33,6 +37,60 @@ export default function NuevoPassword() {
     return ()=> {comprobarToken()}
   }, [])
 
+  const handleSubmit = async e =>{
+    e.preventDefault();
+
+    setAlerta({})
+
+    if([ password, repetirPassword ].includes('')){
+      setAlerta({
+        msg: 'Todos los campos son obligatorios',
+        error: true
+      })
+      return
+    }
+
+    if(password !== repetirPassword){
+      setAlerta({
+        msg: 'Los password no son iguales',
+        error: true
+      })
+      return
+    }
+
+    if(password.length < 6){
+      setAlerta({
+        msg: 'El password es muy corto, agrega mínimo 6 caracteres',
+        error: true
+      })
+      return
+    }
+
+    //Actualizar el password del usuario en la API
+    try {
+      //Accedo directamente al req.body gracias al destructuring a data
+      const url = `${import.meta.env.VITE_BACKEND_URL}/api/usuarios/olvide-password/${token}`;
+      const { data } = await axios.post(url, { password }); 
+
+      setAlerta({
+        msg: data.msg,
+        error: false
+      })
+
+      setPasswordModificado(true);
+
+      //Limpiando el formulario
+      setPassword('');
+      setRepetirPassword('');
+
+    } catch (error) {
+      setAlerta({
+        msg: error.response.data.msg,
+        error: true
+      })
+    }
+  }
+
   const { msg } = alerta;
 
   return (
@@ -44,7 +102,10 @@ export default function NuevoPassword() {
       { msg && <Alerta alerta={alerta}/>}
 
       { tokenValido && (
-        <form className="my-10 bg-white shadow rounded-lg py-5 px-10">
+        <form 
+          className="my-10 bg-white shadow rounded-lg py-5 px-10"
+          onSubmit={handleSubmit}
+        >
         
         <div className="my-5">
           <label
@@ -57,6 +118,8 @@ export default function NuevoPassword() {
             placeholder="Escribe tu nuevo password"
             className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
             id="password"
+            value= {password}
+            onChange = {e => setPassword(e.target.value)}
           />
         </div>
 
@@ -67,16 +130,18 @@ export default function NuevoPassword() {
           >Repetir nuevo password</label>
           
           <input
-            type="password2" 
+            type="password" 
             placeholder="Repite tu nuevo password"
             className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
             id="password2"
+            value={repetirPassword}
+            onChange={ e => setRepetirPassword(e.target.value)}
           />
         </div>
 
         <input
           type="submit"
-          value="Crear cuenta" 
+          value="Cambiar Contraseña" 
           className="bg-sky-700 w-full py-3 text-white uppercase font-bold rounded-xl hover:cursor-pointer hover:bg-sky-800 transition-colors my-4"
         />
 
@@ -84,19 +149,15 @@ export default function NuevoPassword() {
 
     )}
 
-      <nav className="lg:flex lg:justify-between">
-        <Link
-          to={'/'}
-          className='block text-center my-2 text-slate-500 uppercase text-sm'
-        >¿Ya tienes una cuenta? Inicia Sesión
-        </Link>
-
-        <Link
-          to={'/olvide-password'}
-          className='block text-center my-2 text-slate-500 uppercase text-sm'
-        >Olvide mi passwrod
-        </Link>
-      </nav>
+      {passwordModificado && (
+        <nav className="lg:flex lg:justify-between">
+          <Link
+            to={'/'}
+            className='block text-center my-2 text-slate-500 uppercase text-sm'
+          >Inicia Sesión
+          </Link>
+        </nav>
+      )}
     </>
   )
 }
