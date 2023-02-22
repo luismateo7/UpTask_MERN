b14fae0be7edda2629a64bfce0feb54a681a01e7
Mateo Rodriguez <luismateor73@gmail.com>
2023-02-22 03:42:02 -0500

    Mostrar las tareas en caso de existir

--- fronted/src/pages/Proyecto.jsx
+++ fronted/src/pages/Proyecto.jsx
@@ -2,13 +2,12 @@ import { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom"
 import useProyectos from "../hooks/useProyectos";
 import ModalFormularioTarea from "../components/ModalFormularioTarea";
+import Tarea from "../components/Tarea";
 
 export default function Proyecto() {
 
     const params = useParams();
     const { obtenerProyecto, proyecto, cargando, handleModalTarea } = useProyectos();
-
-    const [ modal, setModal ] = useState(false);
     
     useEffect(()=>{
         obtenerProyecto(params.id);
@@ -47,7 +46,21 @@ export default function Proyecto() {
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                   <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zM12.75 9a.75.75 0 00-1.5 0v2.25H9a.75.75 0 000 1.5h2.25V15a.75.75 0 001.5 0v-2.25H15a.75.75 0 000-1.5h-2.25V9z" clipRule="evenodd" />
                 </svg>
-              Nueva Tarea</button>
+                Nueva Tarea
+              </button>
+
+              <p className="font-bold text-xl mt-10">Tareas del Proyecto</p>
+
+              <div className="bg-white shadow mt-10 rounded-lg p-3">
+                {proyecto?.tareas?.length ? 
+                proyecto?.tareas?.map( tarea => (
+                  <Tarea 
+                    key={tarea._id}
+                    tarea={tarea}
+                  />
+                )) :
+                <p className="text-center my-5 p-10">No hay tareas en este proyecto</p>}
+              </div>
 
               <ModalFormularioTarea />
             </>
