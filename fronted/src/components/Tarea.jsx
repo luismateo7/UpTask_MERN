627b801cfafc672951e05f96b69f1c9c7ad7a335
Mateo Rodriguez <luismateor73@gmail.com>
2023-04-30 20:09:21 -0500

    Poner en stare, tarea a eliminar

--- fronted/src/components/Tarea.jsx
+++ fronted/src/components/Tarea.jsx
@@ -1,21 +1,24 @@
+import useProyectos from "../hooks/useProyectos";
 import { formatearFecha } from "../../helpers/formatearFecha";
 
 export default function Tarea({tarea}) {
 
-  const { descripcion, nombre, prioridad, fechaEntrega, _id, estado} = tarea;
+  const { descripcion, nombre, prioridad, fechaEntrega, _id: id, estado} = tarea;
+  const { handleModalEditarTarea } = useProyectos();
 
   return (
     <div className="border-b p-5 flex justify-between">
       <div>
         <p className="mb-1 text-xl">{nombre}</p>
         <p className="mb-1 text-sm text-gray-500 uppercase">{descripcion}</p>
-        <p className="mb-1 text-xl">{formatearFecha(fechaEntrega)}</p>
+        <p className="mb-1 text-sm">{formatearFecha(fechaEntrega)}</p>
         <p className="mb-1 text-xl text-gray-600">{prioridad}</p>
       </div>
 
       <div className="flex gap-2 items-center">
         <button
           className="bg-indigo-600 px-4 py-3 text-white uppercase font-bolf text-sm rounded-lg"
+          onClick={()=> handleModalEditarTarea(tarea)}
         >Editar</button>
 
         { estado ?
