1c20cb12bbe29be3dd9a9ae73550506421553931
Mateo Rodriguez <luismateor73@gmail.com>
2023-05-27 01:01:57 -0500

    Mostrar si eres colaborador o proyecto propio

--- fronted/src/components/PreviewProyecto.jsx
+++ fronted/src/components/PreviewProyecto.jsx
@@ -1,15 +1,27 @@
 import { Link } from "react-router-dom";
-export default function PreviewProyecto({proyecto}) {
+import useAuth from "../hooks/useAuth";
 
-    const { nombre, _id, cliente } = proyecto;
+export default function PreviewProyecto({ proyecto }) {
+
+    const { auth } = useAuth();
+    const { nombre, _id, cliente, creador } = proyecto;
 
     return (
-        <div className="border-b p-5 flex items-center">
-            <p className="flex-1">
-                {nombre}
+        <div className="border-b p-5 flex items-center justify-between">
 
-                <span className="text-sm text-gray-500 uppercase">{` ${cliente}`}</span>
-            </p>
+            <div className="flex items-center gap-2">
+                <p className="flex-1">
+                    {nombre}
+
+                    <span className="text-sm text-gray-500 uppercase">{` ${cliente}`}</span>
+                </p>
+
+                {auth._id !== creador && (
+                    <p className="p-1 text-xs rounded-lg text-white bg-green-500 font-bold uppercase">Colaborador</p>
+                )}
+            </div>
+
+
             <Link
                 to={`${_id}`}
                 className="text-gray-600 hover:text-gray-800 uppercase text-sm font-bold"
