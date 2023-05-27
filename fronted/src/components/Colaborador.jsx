956fc9d1eda94a76a25af04610f2f9c8655b57fc
Mateo Rodriguez <luismateor73@gmail.com>
2023-05-26 21:25:24 -0500

    Mostrar info del colaborador

---
+++ fronted/src/components/Colaborador.jsx
@@ -0,0 +1,25 @@
+import useProyectos from "../hooks/useProyectos"
+
+export default function Colaborador({colaborador}) {
+
+    const { handleModalEliminarColaborador, modalEliminarColaborador } = useProyectos();
+
+    const { nombre, email } = colaborador
+
+    return (
+        <div className="border-b p-5 flex justify-between items-center">
+            <div>
+                <p>{nombre}</p>
+                <p className="text-sm text-gray-700">{email}</p>
+            </div>
+
+            <div>
+                <button
+                    type="button"
+                    className="bg-red-600 px-4 py-3 text-white uppercase font-bold text-sm rounded-lg"
+                    onClick={()=> handleModalEliminarColaborador(colaborador)}
+                >Eliminar</button>
+            </div>
+        </div>
+    )
+}
