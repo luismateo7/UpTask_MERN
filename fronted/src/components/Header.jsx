b65b0684150ed495fc0b41540d82a77a2a62406b
Mateo Rodriguez <luismateor73@gmail.com>
2023-05-28 20:28:22 -0500

    Redirect a proyectos

--- fronted/src/components/Header.jsx
+++ fronted/src/components/Header.jsx
@@ -17,7 +17,9 @@ export default function Header() {
   return (
     <header className="px-4 py-5 border-b bg-white">
       <div className="md:flex md:justify-between">
-        <h2 className="text-4xl text-center text-sky-600 font-black mb-5 md:mb-0">Uptask</h2>
+        <Link to={'/proyectos'}>
+          <h2 className="text-4xl text-center text-sky-600 font-black mb-5 md:mb-0">Uptask</h2>
+        </Link>
 
         <div className="flex flex-col md:flex-row gap-4 items-center">
 
